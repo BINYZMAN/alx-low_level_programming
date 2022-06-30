4b3188677c73e156e7907c5fe4f324e99f6f9e12
BINYZMAN <Binyzman19@gmail.com>
2022-06-30 10:25:33 -0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-char str[] = "\"""Programming is like ";
-str[]="building a multilingual puzzle";
-puts(str); 
+char str[] = "\"""Programming is like";
+char str2[] = "building a multilingual puzzle";
+puts(str,str2); 
 return (0);
 }
