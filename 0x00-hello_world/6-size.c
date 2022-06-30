5d024dcddb3059f39319d102dc41a33dbf7e09ba
BINYZMAN <Binyzman19@gmail.com>
2022-06-30 13:51:45 -0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int intType;
 long int longintType;
 long long int longlongintType;
 float floatType;
-printf("Size of char: %zu bytes\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long int: %zu bytes\n", sizeof(longintType));
-printf("Size of long long int: %zu bytes\n", sizeof(longlongintType);
-printf("Size of float: %zu byte\n", sizeof(floatType));
+printf("Size of char: %lu bytes\n", sizeof(charType));
+printf("Size of int: %lu bytes\n", sizeof(intType));
+printf("Size of long int: %lu bytes\n", sizeof(longintType));
+printf("Size of long long int: %lu bytes\n", sizeof(longlongintType);
+printf("Size of float: %lu byte\n", sizeof(floatType));
 return (0);
 }
