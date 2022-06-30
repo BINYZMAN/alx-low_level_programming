4d2f4eaf5cf7fe256daef761b8f93e2489484e8b
BINYZMAN <Binyzman19@gmail.com>
2022-06-30 10:46:49 -0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/**
+
+int main(void)
+	/**
 	 * This is the preferred style for multi-line
 	 * comments in C source code.
 	 * Please use it consistently.
@@ -7,7 +9,6 @@
 	 * Description:  A code that prints message,
 	 * with beginning and ending almost-blank lines.
 	 */
-int main(void)
 {
 char str[] = "\"""Programming is like building a multilingual puzzle";
 puts(str); 
