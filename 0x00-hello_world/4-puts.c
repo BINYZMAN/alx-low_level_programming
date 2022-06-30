7e29c66bca653f51fa6799fee76f1741e2c6532b
BINYZMAN <Binyzman19@gmail.com>
2022-06-30 10:05:52 -0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
-
 #include <stdio.h>
-#include <string.h>
 
 int main (void)
 {
-  /*declaring and initalazing variable*/
-  char str[]= "\"""Programming is like building a multilingual puzzle";
-  /*printing output*/
-  puts(str); 
-  return (0);
+    char str[]= "\"""Programming is like building a multilingual puzzle";
+    puts(str); 
+    return (0);
 }
