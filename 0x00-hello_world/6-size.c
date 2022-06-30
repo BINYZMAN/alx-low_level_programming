513efe285a58fc4f8f74a2685eb9cd71be78d80f
BINYZMAN <Binyzman19@gmail.com>
2022-06-30 13:48:41 -0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,19 @@
-/*my first code in c*/
 #include <studio.h>
-
-int main()
+/**
+* main -main block
+* Return:0
+*/
+int main(void)
 {
-	/*declaring variables */
-	char charType;
-	int intType;
-	long int longintType;
-	long long int longlongintType;
-	float floatType;
-
-	/*print out puts*/
-	    printf("Size of char: %zu bytes\n", sizeof(charType));
-	    printf("Size of int: %zu bytes\n", sizeof(intType));
-	    printf("Size of long int: %zu bytes\n", sizeof(longintType));
-      	   printf("Size of long long int: %zu bytes\n", sizeof(longlongintType);
-            printf("Size of float: %zu byte\n", sizeof(floatType));
-
-	    return (0);
+char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+printf("Size of char: %zu bytes\n", sizeof(charType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of long int: %zu bytes\n", sizeof(longintType));
+printf("Size of long long int: %zu bytes\n", sizeof(longlongintType);
+printf("Size of float: %zu byte\n", sizeof(floatType));
+return (0);
 }
