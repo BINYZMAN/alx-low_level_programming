f218bfb372ac1dd5436c48502327dd5b0e0563ac
BINYZMAN <Binyzman19@gmail.com>
2022-07-01 04:47:56 -0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,14 @@
 int main(void)
 {
 char ch;
-for (ch= ‘a’; ch<= ‘z’; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 if (ch=='z')
 {
-for (ch= ‘A’; ch<= ‘Z’; ch++)
+for (ch = ‘A’; ch <= ‘Z’; ch++)
 putchar(ch);
 }
 }
-getch();
 return (0);
 }
