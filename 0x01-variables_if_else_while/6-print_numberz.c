7ee8d2483311b2b171f9fa06427dce7ffa24f1b0
BINYZMAN <Binyzman19@gmail.com>
2022-07-01 04:38:12 -0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /*
- * main-main block
- * Return:0
- */
+* main-main block
+* Return:0
+*/
 int main(void)
 {
 char ch;
 ch=0;
 while (ch<10)
 {
-putchar('ch');
+putchar(ch);
 ch++;
 }
 return (0);
