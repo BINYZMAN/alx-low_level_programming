d237fa1b0bbd86f3e803f657ff2b4f807bdac976
BINYZMAN <Binyzman19@gmail.com>
2022-07-01 15:45:31 -0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,20 +5,17 @@
 */
 int main(void)
 {
-int ch;
-ch = 0;
 for (int i = 0; i <= 9; i++)
 {
-putchar((ch)+'0);
+putchar((i) + '0);
 for (int j = 0; j <= 9; j++)
 {
-if (j>i)
+if (j > i)
 {
-putchar((j)+'0');
+putchar((j) + '0');
 putchar(', ');
 }
 }
-ch++;
 }
 putchar('\n');
 return (0);
