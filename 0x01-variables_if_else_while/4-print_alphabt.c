e840f5107e5de27451f06722a67427f8fcb47432
BINYZMAN <Binyzman19@gmail.com>
2022-07-01 08:04:55 -0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
 * main -main block
 * Return:0
 */
@@ -8,8 +8,9 @@ int main(void)
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-if(ch != 'e' && ch != 'q')
+if (ch != 'e' && ch != 'q')
 putchar(ch);
 }
+putchar('\n');
 return (0);
 }
