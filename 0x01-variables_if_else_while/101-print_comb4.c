f27ec82d6e92b4ea897b760c56e6fa9227825496
binyzman <binyzman19@gmail.com>
2022-07-01 13:07:33 -0700

    adding project 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+* main -main block
+* Return:0
+*/
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (j > i)
+{
+if (k > j)
+{
+for (k = 0; k <= 9; k++)
+{
+putchar((i) + '0');
+putchar((j) + '0');
+putchar((k) + '0');
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
