e9f5304866fd5b5bf2d46f4b1e84ba3f83be5785
binyzman <binyzman19@gmail.com>
2022-07-01 13:55:48 -0700

    adding project 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main -main block
+* Return:0
+*/
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+for (k = 0; k <= 9; k++)
+{
+for (l = 0; l <= 9; l++)
+{
+putchar((k) + '0');
+putchar((l) + '0');
+putchar(32);
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+putchar((i) + '0');
+putchar((j) + '0');
+putchar(44);
+}
+}
+}
+return (0);
+}
