a3954f1f6f9551adb9886a157321e106a7ac5898
BINYZMAN <Binyzman19@gmail.com>
2022-07-01 08:27:59 -0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/*
+/**
 * main -main block
 * Return:0
 */
 int main(void)
 {
 char ch;
-for (ch = 'z'; ch <= 'a'; ch--)
+for (ch = 'z'; ch >= 'a'; ch--)
 putchar(ch);
 return (0);
 }
