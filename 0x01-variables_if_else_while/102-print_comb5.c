e865dc67a1ab051c862e16db3ebdb1bcc34ae7f8
BINYZMAN <Binyzman19@gmail.com>
2022-07-03 04:52:20 -0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,8 +14,6 @@ for (k = 0; k <= 9; k++)
 {
 for (l = 0; l <= 9; l++)
 {
-if (k == 9 && l == 9)
-break;
 for (i = k; i <= 9; i++)
 {
 for (j = l; j <= 9; j++)
@@ -27,11 +25,11 @@ if (count == 1)
 j++;
 count = 0;
 }
-putchar((k) + '0');
-putchar((l) + '0');
+putchar((k) +'0');
+putchar((l) +'0');
 putchar(32);
-putchar((i) + '0');
-putchar((j) + '0');
+putchar((i) +'0');
+putchar((j) +'0');
 if (k == 9 && l == 8 && i == 9 && j == 9)
 {
 }
