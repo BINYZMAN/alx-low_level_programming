bf31f0f83098e386b57c5a9f00b0f48a6402f4d3
BINYZMAN <Binyzman19@gmail.com>
2022-07-06 17:25:06 -0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,16 @@
 /**
 * print_alphabet_x10 - this function prints a to z 10 times in different lines
 *
-* Return: 0
+* Returns : 0
 */
-void print_alphabet_x10(void)
+void printalphabet_x10(void)
 {
-int i = 0;
+int i=0;
 int j;
-while (i < 10)
+while(i < 10)
 {
-j = 97;
-while (j < 123)
+j=97;
+while(j < 123)
 {
 _putchar(j);
 j++;
