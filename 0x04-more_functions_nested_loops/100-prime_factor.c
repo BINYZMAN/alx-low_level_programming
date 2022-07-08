321a346477bd99fcecb5a364652298ba0690d09b
binyzman <binyzman19@gmail.com>
2022-07-08 12:40:57 -0700

    adding project files

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - identifies the largest prime factor of a given number
+* Return: 0
+*/
+int main(void)
+{
+long int i, n = 612852475143;
+i = 2;
+while (i < n)
+{
+if (n % i == 0)
+{
+n = n / i;
+}
+i++;
+}
+printf("%ld\n", i);
+return (0);
+}
