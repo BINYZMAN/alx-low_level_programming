1d757f1c62fa65cc95aa9a9c6c88af1d31fda0fb
BINYZMAN <Binyzman19@gmail.com>
2022-07-12 15:42:32 -0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,16 @@
-fjsgdvjhdsgjhgj
+#include "main.h"
+
+/**
+ * _puts- prints a string
+ * @str: pointer to the string
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += sizeof(char);
+	}
+	_putchar('\n');
+}
