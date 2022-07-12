6d0bc2d2af97281a91645ad57cdbbe2084125bc6
BINYZMAN <Binyzman19@gmail.com>
2022-07-12 15:43:18 -0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,24 @@
-jfdjhgdsjhgfsh
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @str: string given
+ *
+ */
+
+void print_rev(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		_putchar(str[i - 1]);
+		i--;
+	}
+	_putchar('\n');
+}
