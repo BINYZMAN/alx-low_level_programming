c06f48ef3b39559715e5cc551dcb50fac4f299fe
BINYZMAN <Binyzman19@gmail.com>
2022-07-12 15:46:20 -0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,34 @@
-wetrafjzhdcbj
+#include "main.h"
+
+/**
+ * puts_half - prints half of a srting
+ * @str: given string
+ */
+
+void puts_half(char *str)
+{
+	int c, half;
+	char *str2;
+
+	c = 0, half = 0;
+	str2 = str;
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	if (c % 2 == 0)
+	{
+		half = c / 2;
+	}
+	else if (c % 2)
+	{
+		half = ((c - 1) / 2) + 1;
+	}
+	c = half;
+	while (str2[c] != '\0')
+	{
+		_putchar(str2[c]);
+		c++;
+	}
+	_putchar('\n');
+}
