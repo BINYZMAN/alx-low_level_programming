a1a59d5f64528271736b2d2d424b79f7a1d4bf73
BINYZMAN <Binyzman19@gmail.com>
2022-07-12 15:44:13 -0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,29 @@
-fhgadfjhadfgajhd
+#include "main.h"
+
+/**
+ * rev_string - print string in reverse
+ * @s: string given
+ *
+ */
+
+void rev_string(char *s)
+{
+	char *t = s;
+	char tmp[1000];
+	short c = 0;
+
+	while (*s != '\0')
+	{
+		tmp[c] = *s;
+		s++;
+		c++;
+	}
+	c = 0;
+
+	while (s > t)
+	{
+		s--;
+		*s = tmp[c];
+		c++;
+	}
+}
