c62c0655f9f485ea0a124cecff4a848dd7570964
BINYZMAN <Binyzman19@gmail.com>
2022-07-15 06:18:57 -0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,24 @@
-hfchjasgfhjwgfwawfg
+#include "main.h"
+
+/**
+ * *cap_string - this is awesome
+ * @s: pointer to char params
+ *
+ * Return: *s
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char d[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+				for (j = 0; d[j] != '\0'; j++)
+					if (s[i] == d[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
+	}
+	return (s);
+}
