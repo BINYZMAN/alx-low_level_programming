0bdcbf269e91a07e63a0ea067df50212cc6cda87
BINYZMAN <Binyzman19@gmail.com>
2022-07-20 15:56:39 -0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,13 +5,13 @@
  * @s: string given
  *
  */
-
-void _puts_recursion(char *s)
-{
-	int _putchar(char c)
+int _putchar(char c)
 {
     return (write(1, &c, 1));
 }
+
+void _puts_recursion(char *s)
+{
 	if (*s == '\0')
 	{
 		_putchar('\n');
