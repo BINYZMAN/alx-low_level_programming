eeedac69936029a33b157bb0e312d2e009c2c1b9
BINYZMAN <Binyzman19@gmail.com>
2022-07-26 06:02:28 -0300

    Create 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_array - allocates a memory space and fills it with given character.
+ * @size:size if memory space
+ * @c: character given
+ *
+ * Return: null or memolry space allocated
+ *
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i = 0;
+
+	arr = (char *) malloc(sizeof(char) * size);
+
+	if (size == 0 || arr == 0)
+	{
+		return ('\0');
+	}
+	else if (size > 0)
+	{
+		while (i < size)
+		{
+			arr[i] = c;
+			i++;
+		}
+		return (arr);
+	}
+	else
+	{
+		return ('\0');
+	}
+}
