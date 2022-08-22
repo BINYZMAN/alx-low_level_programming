660707b7050a2d98715b8b47ea74cc2497a57936
BINYZMAN <Binyzman19@gmail.com>
2022-08-22 16:17:31 -0300

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads text file and prints it to POSIX strandard output.
+ * @filename: file name
+ * @letters: number of letters it should read and print
+ *
+ * Return: number of letters it could read and print
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}om.facebook.orca;FBLC/en_GB;FBBV/363453956;FBCR/Ethio
